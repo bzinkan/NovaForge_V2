f0d80e2685c0edc5180cff6f90c2e481c49bc12e
bzinkan <zinkan.brian@gmail.com>
2025-12-18 12:56:39 -0500

    Add Unity package structure for NoveForge V2

---
+++ Packages/com.bzinkan.noveforge/Runtime/Models/NoveForgeAsset.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace NoveForge.Runtime.Models
+{
+    [Serializable]
+    public class NoveForgeAsset
+    {
+        public string assetId;
+        public string assetType;
+        public string downloadUrl;
+    }
+}
